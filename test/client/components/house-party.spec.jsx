2e6820474c40faf4a2b5ae078847466a3490a261
James McIntyre <jmcintyre@walmartlabs.com>
2016-09-20 15:38:57 -0700

    fix component test

--- test/client/components/house-party.spec.jsx
+++ test/client/components/house-party.spec.jsx
@@ -8,10 +8,26 @@ import HouseParty from "src/components/house-party";
 
 describe("components/resource-list", () => {
 
+  let invitees;
+  let friends;
+
+  beforeEach(() => {
+    friends = [
+      {name: "electrode", img: "//goo.gl/I9utJF", size: 12},
+      {name: "node", img: "//goo.gl/hxmCEE", size: 12},
+      {name: "React", img: "//goo.gl/xwbqlB", size: 12}
+    ];
+    invitees = [
+      {name: "electrode", invited: false},
+      {name: "node", invited: false},
+      {name: "React", invited: false}
+    ];
+  });
+
   describe("Mounting", () => {
 
     it("should render into the document", () => {
-      const component = shallow(<HouseParty />);
+      const component = shallow(<HouseParty ourFriends={friends} invitees={invitees}/>);
       expect(component).to.not.be.null;
     });
 
